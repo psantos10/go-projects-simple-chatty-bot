5d744bb2b4159908ecd31fc92c033b9faad0ea05
Patrício dos Santos <hello@psantos.dev>
2023-08-18 18:21:02 +0100

    Stage 3-5: Guess the age

--- main.go
+++ main.go
@@ -11,4 +11,16 @@ func main() {
 	fmt.Scanln(&name)
 
 	fmt.Printf("What a great name you have, %s!\n", name)
+
+	fmt.Println("Let me guess your age.")
+	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+	var rem3, rem5, rem7 int
+	fmt.Scan(&rem3)
+	fmt.Scan(&rem5)
+	fmt.Scan(&rem7)
+
+	age := (rem3*70 + rem5*21 + rem7*15) % 105
+
+	fmt.Printf("Your age is %d; that's a good time to start programming!\n", age)
 }
