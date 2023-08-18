32d052188ecc8be411087a7770e83811003c7564
Patrício dos Santos <hello@psantos.dev>
2023-08-18 18:31:13 +0100

    Stage 5-5: Multiple choice

--- main.go
+++ main.go
@@ -1,37 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main() {
-	fmt.Println("Hello! My name is Aid.")
-	fmt.Println("I was created in 2020.")
-	fmt.Println("Please, remind me your name.")
+func greet(name, year string) {
+	fmt.Println("Hello! My name is " + name + ".")
+	fmt.Println("I was created in " + year + ".")
+}
 
+func showName() {
 	var name string
-	fmt.Scanln(&name)
+	fmt.Println("Please, remind me your name.")
+	fmt.Scan(&name)
+	fmt.Println("What a great name you have, " + name + "!")
+}
 
-	fmt.Printf("What a great name you have, %s!\n", name)
+func guessAge() {
+	var rem3, rem5, rem7, age int
 
 	fmt.Println("Let me guess your age.")
 	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
+	fmt.Scan(&rem3, &rem5, &rem7)
 
-	var rem3, rem5, rem7 int
-	fmt.Scan(&rem3)
-	fmt.Scan(&rem5)
-	fmt.Scan(&rem7)
+	age = (rem3*70 + rem5*21 + rem7*15) % 105
+	fmt.Println("Your age is " + strconv.Itoa(age) + "; that's a good time to start programming!")
+}
 
-	age := (rem3*70 + rem5*21 + rem7*15) % 105
-
-	fmt.Printf("Your age is %d; that's a good time to start programming!\n", age)
+func count() {
+	var n int
 
 	fmt.Println("Now I will prove to you that I can count to any number you want.")
+	fmt.Scan(&n)
+	for i := 0; i <= n; i++ {
+		fmt.Printf("%d!\n", i)
+	}
+}
 
-	var num int
-	fmt.Scanln(&num)
+func startQuiz() {
+	var answer int
 
-	for i := 0; i <= num; i++ {
-		fmt.Printf("%d !\n", i)
+	fmt.Println("Let's test your programming knowledge.")
+	fmt.Scanln(&answer)
+
+	for answer != 2 {
+		fmt.Println("Please, try again.")
+		fmt.Scanln(&answer)
 	}
 
 	fmt.Println("Completed, have a nice day!")
+}
+
+func sayGoodbye() {
+	fmt.Println("Congratulations, have a nice day!")
+}
+
+func main() {
+	greet("Aid", "2020")
+	showName()
+	guessAge()
+	count()
+	startQuiz()
+	sayGoodbye()
 }
