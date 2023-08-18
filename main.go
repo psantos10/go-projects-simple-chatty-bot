561d55687959c1e766f0ecbe7f0896a14b157328
Patrício dos Santos <hello@psantos.dev>
2023-08-18 18:14:26 +0100

    Stage 2-5: Print your name

--- main.go
+++ main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello! My name is Aid.")
 	fmt.Println("I was created in 2020.")
+	fmt.Println("Please, remind me your name.")
+
+	var name string
+	fmt.Scanln(&name)
+
+	fmt.Printf("What a great name you have, %s!\n", name)
 }
